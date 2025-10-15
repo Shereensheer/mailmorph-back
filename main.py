a21f1fbdd1475e06cb7ca47dce34cb9f312e3aa8
Shereen Ahmed <165431743+Shereensheer@users.noreply.github.com>
2025-10-15 16:51:30 +0500

    main.py

--- main.py
+++ main.py
@@ -1161,32 +1161,19 @@ def clear_replies():
 
 
 
-# import os
-# import uvicorn
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# @app.get("/")
-# def home():
-#     return {"message": "MailMorph backend running successfully on Railway!"}
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 8000))
-#     uvicorn.run("main:app", host="0.0.0.0", port=port)   
-
-
 import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "✅ MailMorph backend running successfully on Railway!"}
+    return {"message": "MailMorph backend running successfully on Railway!"}
 
 if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8080))  # Railway uses 8080
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)   
+
+
 
